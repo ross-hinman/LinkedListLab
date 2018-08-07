845204c785c3ad25db0ec8902930c782893ba5a7
Brian Gardner <1761566+briangardner@users.noreply.github.com>
2018-08-07 16:43:46 -0400

    Added code for normal and generic linked list.  Added code to show how the list works

---
+++ LinkedList/LinkedList.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    //Non-Generic LinkedList
+    class LinkedList
+    {
+        public LinkedList()
+        {
+            Head = new Node();
+            Current = Head;
+        }
+
+        public Node Head;
+        public Node Current;
+        public int Count = 0;
+
+        public void AddAtStart(object value)
+        {
+            var newNode = new Node()
+            {
+                Value = value
+            };
+            newNode.Next = Head.Next;
+            Head.Next = newNode;
+            ++Count;
+        }
+
+        public void RemoveFromStart()
+        {
+            if (Count <= 0)
+            {
+                throw new Exception("There are no elements to remove");
+            }
+
+            Head.Next = Head.Next.Next;
+            --Count;
+        }
+
+        public void PrintAllNodes()
+        {
+            Console.Write("Head -> ");
+            Node curr = Head;
+            while (curr.Next != null)
+            {
+                curr = curr.Next;
+                Console.Write(curr.Value); //If using a reference type (any class/interface), you will need to override ToString for this to work.
+                Console.Write(" -> ");
+            }
+            Console.Write("NULL");
+            Console.WriteLine();
+        }
+
+
+    }
+
+    //Linked List using Generics
+    class LinkedList<T> where T : class
+    {
+        public LinkedList()
+        {
+            Head = new Node<T>();
+            Current = Head;
+        }
+        public Node<T> Head;
+        public Node<T> Current;
+        public int Count = 0;
+
+        public void AddAtStart(T value)
+        {
+            var newNode = new Node<T>()
+            {
+                Value = value
+            };
+            newNode.Next = Head.Next;
+            Head.Next = newNode;
+            ++Count;
+        }
+
+        public void RemoveFromStart()
+        {
+            if (Count <= 0)
+            {
+                throw new Exception("There are no elements to remove");
+            }
+
+            Head.Next = Head.Next.Next;
+            --Count;
+        }
+
+        public void PrintAllNodes()
+        {
+            Console.Write("Head -> ");
+            Node<T> curr = Head;
+            while (curr.Next != null)
+            {
+                curr = curr.Next;
+                Console.Write(curr.Value);
+                Console.Write(" -> ");
+            }
+            Console.Write("NULL");
+            Console.WriteLine();
+        }
+    }
+
+    
+    
+}
--- LinkedList/Program.cs
+++ LinkedList/Program.cs
@@ -8,8 +8,103 @@ namespace LinkedList
 {
     class Program
     {
+        private static readonly List<string> Classmates = new List<string>()
+        {
+            "Rabin",
+            "Aquoinette",
+            "Chris",
+            "Sean A",
+            "Sean S",
+            "Ross",
+            "Dr. Clark",
+            "Mike",
+            "Catherine",
+            "Bradley",
+            "Jacob"
+        };
         static void Main(string[] args)
         {
+            RunLinkedListExample();
+            Console.WriteLine("Press any key to continue to Generic Linked List example");
+            Console.ReadKey();
+            Console.Clear();
+            RunGenericLinkedListExample();
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+
+        private static void RunGenericLinkedListExample()
+        {
+            Console.WriteLine("=================================");
+            Console.WriteLine("Generic Linked List Example");
+            Console.WriteLine("=================================");
+            var genericLinkedList = new LinkedList<string>();
+            genericLinkedList.PrintAllNodes();
+            foreach (var student in Classmates)
+            {
+                genericLinkedList.AddAtStart(student);
+                Console.Write($"After adding {student}: ");
+                genericLinkedList.PrintAllNodes();
+            }
+
+            Console.WriteLine("Press any key to continue.");
+            Console.ReadKey();
+            Console.WriteLine();
+            Console.WriteLine("******** Removing Classmates ***********");
+            while (true)
+            {
+                try
+                {
+                    genericLinkedList.RemoveFromStart();
+                    genericLinkedList.PrintAllNodes();
+                }
+                catch (Exception)
+                {
+                    genericLinkedList.PrintAllNodes();
+                    break;
+                }
+            }
+
+            Console.WriteLine("Press any key to continue.");
+            Console.ReadKey();
+            Console.WriteLine();
+        }
+
+        private static void RunLinkedListExample()
+        {
+            Console.WriteLine("=================================");
+            Console.WriteLine("Linked List Example");
+            Console.WriteLine("=================================");
+            var objectLinkedList = new LinkedList();
+            objectLinkedList.PrintAllNodes();
+            foreach (var student in Classmates)
+            {
+                objectLinkedList.AddAtStart(student);
+                Console.Write($"After adding {student}: ");
+                objectLinkedList.PrintAllNodes();
+            }
+
+            Console.WriteLine("Press any key to continue.");
+            Console.ReadKey();
+            Console.WriteLine();
+            Console.WriteLine("******** Removing Classmates ***********");
+            while (true)
+            {
+                try
+                {
+                    objectLinkedList.RemoveFromStart();
+                    objectLinkedList.PrintAllNodes();
+                }
+                catch (Exception)
+                {
+                    objectLinkedList.PrintAllNodes();
+                    break;
+                }
+            }
+
+            Console.WriteLine("Press any key to continue.");
+            Console.ReadKey();
+            Console.WriteLine();
         }
     }
 }
